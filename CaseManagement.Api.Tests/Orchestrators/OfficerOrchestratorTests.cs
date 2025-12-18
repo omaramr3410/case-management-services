bd027b84abf45ba4c9bebcf6f120aab157c8f75f
omaramr3410 <omaramr3410@gmail.com>
2025-12-18 14:11:24 -0500

    adding api test solution project

---
+++ CaseManagement.Api.Tests/Orchestrators/OfficerOrchestratorTests.cs
@@ -0,0 +1,33 @@
+﻿using CaseManagement.Api.Domain.DTOs.Officers;
+using CaseManagement.Api.Infrastructure.Auditing;
+using CaseManagement.Api.Infrastructure.Repositories;
+using CaseManagement.Api.Infrastructure.Security;
+using CaseManagement.Api.Orchestrators;
+using NSubstitute;
+using Xunit;
+
+public class OfficerOrchestratorTests
+{
+    private readonly IOfficerRepository _repo = Substitute.For<IOfficerRepository>();
+    private readonly IAuditService _audit = Substitute.For<IAuditService>();
+
+    [Fact]
+    public async Task CreateAsync_ShouldCreateOfficer()
+    {
+        var orchestrator = new OfficerOrchestrator(_repo, _audit);
+
+        var request = new OfficerCreateRequest
+        {
+            FirstName = "Jane",
+            LastName = "Smith",
+            Region = "VA"
+        };
+
+        var userContext = new UserContext { UserId = Guid.Parse("CD1EF5BF-3BD4-49F9-1FB5-08DE3CDD3A2C"), Username = "adminTestRole", IpAddress = "TESTING", UserRole = "admin" };
+
+        var result = await orchestrator.CreateAsync(request, userContext);
+
+        Assert.NotNull(result);
+        await _audit.Received(1).LogAsync(userContext,"Officer", result.ToString(), "CREATE");
+    }
+}
